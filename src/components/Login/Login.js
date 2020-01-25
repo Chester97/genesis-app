96e31fbc40e393d5d9c2eb98465f41930a29788c
Kamil Kowalczuk-Fijałkowski <kamil.fijalkowski@intive.com>
2020-01-26 00:05:35 +0100

    Add style file to Login component, change Login Service

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,64 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { loginUser } from '../../services/LoginService';
-
-const LoginWrapper = styled.section`
-    height: 100vh;
-    background: #1B1B1B;
-    color: #F2F3F4;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 40px 20px;
-`;
-
-const LoginMainText = styled.h1`
-    text-align: center;
-    font-size: 32px;
-`;
-
-const LoginForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 80px;
-    min-width: 70vw;
-`;
-
-const FormItem = styled.input`
-    margin: 0 0 30px 0;
-    border: none;
-    border-bottom: 1px solid white;
-    background: none;
-    color: #F2F3F4;
-    padding: 0 0 5px 5px;
-    opacity: .5;
-
-    &:focus {
-        opacity: 1;
-    }
-`;
-
-const FormButton = styled.button`
-    background: transparent;
-    border: 2px solid #F2F3F4;
-    border-radius: 4px;
-    color: #F2F3F4;
-    opacity: .5;
-    padding: 15px 0;
-
-    &:hover,
-    &:focus {
-        opacity: 1;
-    }
-`;
-
-const ErrorMessage = styled.h1`
-    color: #FFD2D2;
-    text-transform: uppercase;
-    margin-top: 60px;
-    text-align: center;
-`;
+import * as S from './styles';
+import { loginService } from '../../services/LoginService';
 
 const Login = () => {
     const formInitialState = { login: "", password: "" };
@@ -75,7 +17,7 @@ const Login = () => {
 
     const handleSubmit = async(e) => {
         e.preventDefault();
-        let userData = await loginUser(formState);
+        let userData = await loginService.loginUser(formState);
         if(userData && userData.message) {
             setErrors(userData.message); 
             return;
@@ -85,17 +27,17 @@ const Login = () => {
     }
 
     return (
-        <LoginWrapper>
-            <LoginMainText>Login Form</LoginMainText>
-                <LoginForm onSubmit={handleSubmit}>
-                    <FormItem type="text" name="login" placeholder="Your Login" onChange={handleChange}/>
-                    <FormItem type="text" name="password" placeholder="Your password" onChange={handleChange}/>
-                    <FormButton type="submit">Send</FormButton>
-                </LoginForm>
+        <S.LoginWrapper>
+            <S.LoginMainText>Login Form</S.LoginMainText>
+                <S.LoginForm onSubmit={handleSubmit}>
+                    <S.FormItem type="text" name="login" placeholder="Your Login" onChange={handleChange}/>
+                    <S.FormItem type="text" name="password" placeholder="Your password" onChange={handleChange}/>
+                    <S.FormButton type="submit">Send</S.FormButton>
+                </S.LoginForm>
                 {
-                    errors ? <ErrorMessage>{errors}</ErrorMessage> : null
+                    errors ? <S.ErrorMessage>{errors}</S.ErrorMessage> : null
                 }
-        </LoginWrapper>
+        </S.LoginWrapper>
     )
 }
 
--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -2,8 +2,14 @@ import { BASE_URL, createRequestData } from './utils';
 
 const LOGIN_URL = `${BASE_URL}user/login`;
 
-export const loginUser = async (body) => {
-  const userResponse = await fetch(LOGIN_URL, createRequestData('POST', body));
-  const userData = await userResponse.json();
-  return userData;
+export const loginService = {
+  loginUser: async (body) => {
+    try{
+      const response = await fetch(LOGIN_URL, createRequestData('POST', body));
+      const responseData = await response.json();
+      return responseData;
+    }catch(e) {
+      return e;
+    }
+  }
 }
