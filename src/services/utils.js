b96b91157f50f09081484d228f9dd4021517883f
Kamil Kowalczuk-Fijałkowski <kamil.fijalkowski@intive.com>
2020-01-26 23:08:16 +0100

    Implement airbnb styles guid with esLint

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,13 +1,10 @@
 const REQUEST_CONFIG = {
   crossDomain: true,
-  headers: {
-    "Content-Type": "application/json"
-  },
-}
+  headers: { 'Content-Type': 'application/json' },
+};
 
 export const createRequestData = (method, body) => ({
   ...REQUEST_CONFIG,
   method,
   body: JSON.stringify(body),
 });
-
